cf442344b721bdd1fd05e12c3bfe3c0b8a278037
GwiHwan-Go <iejw1914@gmail.com>
2022-05-13 08:29:23 +0000

    refactoring folder

--- Data/BengaliDataset.py
+++ Data/BengaliDataset.py
@@ -45,5 +45,5 @@ if __name__ == "__main__" :
                             img_height=137,
                             img_width=236
                             )
-    print(img, arr=dataset[0])
+    print(dataset[0])
     print(len(dataset))
